4249b0ccd4a3cc1b34e393cd796939b41b4d9b24
Artem Lazarchuk <la.artem.ser@gmail.com>
2026-04-24 09:20:23 +0300

    Added Postgres connection 'TestIvyInit'.

---
+++ Connections/TestIvyInit/Models/IvyAskQuestion.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace TestIvyInit.Connections.TestIvyInit.Models;
+
+[Table("ivy_ask_questions")]
+public partial class IvyAskQuestion
+{
+    [Key]
+    public Guid Id { get; set; }
+
+    [StringLength(100)]
+    public string Widget { get; set; } = null!;
+
+    [StringLength(100)]
+    public string Category { get; set; } = null!;
+
+    [StringLength(10)]
+    public string Difficulty { get; set; } = null!;
+
+    public string QuestionText { get; set; } = null!;
+
+    [StringLength(20)]
+    public string Source { get; set; } = null!;
+
+    public DateTime CreatedAt { get; set; }
+
+    public bool IsActive { get; set; }
+
+    [InverseProperty("Question")]
+    public virtual ICollection<IvyAskTestResult> IvyAskTestResults { get; set; } = new List<IvyAskTestResult>();
+}
---
+++ Connections/TestIvyInit/Models/IvyAskTestRun.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace TestIvyInit.Connections.TestIvyInit.Models;
+
+[Table("ivy_ask_test_runs")]
+[Index("IvyVersion", Name = "ix_test_runs_ivy_version")]
+public partial class IvyAskTestRun
+{
+    [Key]
+    public Guid Id { get; set; }
+
+    [StringLength(50)]
+    public string IvyVersion { get; set; } = null!;
+
+    [StringLength(50)]
+    public string Environment { get; set; } = null!;
+
+    public int TotalQuestions { get; set; }
+
+    public int SuccessCount { get; set; }
+
+    public int NoAnswerCount { get; set; }
+
+    public int ErrorCount { get; set; }
+
+    public DateTime StartedAt { get; set; }
+
+    public DateTime? CompletedAt { get; set; }
+
+    [StringLength(20)]
+    public string DifficultyFilter { get; set; } = null!;
+
+    [StringLength(10)]
+    public string Concurrency { get; set; } = null!;
+
+    [InverseProperty("TestRun")]
+    public virtual ICollection<IvyAskTestResult> IvyAskTestResults { get; set; } = new List<IvyAskTestResult>();
+}
